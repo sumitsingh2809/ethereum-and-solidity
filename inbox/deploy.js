b117998d43bad85ce12883b26885dcefaaba917b
Sumit Singh <sumitsingh@Rentshers-MacBook-Air.local>
2021-12-11 17:16:47 +0530

    delopy code

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,22 @@
-// deploy code will go here
+require('dotenv').config();
+const Web3 = require('web3');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { interface, bytecode } = require('./compile');
+const { MNEMONIC_PHRASE, PROVIDER_OR_URL } = process.env;
+
+const provider = new HDWalletProvider(MNEMONIC_PHRASE, PROVIDER_OR_URL);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi There!'] })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+}
+deploy().catch(err => {console.error(err)})
