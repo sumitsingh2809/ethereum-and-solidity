e44375a9d5d19fd7dbf216ff333656287016cfb2
Sumit Singh <sumitsingh@Rentshers-MacBook-Air.local>
2021-12-11 13:55:36 +0530

    added test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,6 +1,6 @@
+const Web3 = require('web3');
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3');
 const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
@@ -26,5 +26,11 @@ describe('Inbox', () => {
     it('has a default message', async () => {
         const message = await inbox.methods.message().call();
         assert.equal(message, INITIAL_STRING);
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     });
 });
